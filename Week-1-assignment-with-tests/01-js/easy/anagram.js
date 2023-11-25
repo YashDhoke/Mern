498da6dc0a435c3422c076c104d8858fb7d21a76
YashDhoke <yashdhoke1@gmail.com>
2023-11-25 16:47:17 +0530

    easy solved

--- Week-1-assignment-with-tests/01-js/easy/anagram.js
+++ Week-1-assignment-with-tests/01-js/easy/anagram.js
@@ -7,9 +7,45 @@
   - `npm run test-anagram`
 */
 
+let NO_OF_CHARS = 256;
+
 function isAnagram(str1, str2)
 {
-   return yes ; 
+    let count = new Array(NO_OF_CHARS) ; 
+    let s1 = str1.toLowerCase();
+    let s2 = str2.toLowerCase();
+
+    for(let i = 0 ; i < NO_OF_CHARS ; i++) 
+    {
+      count[i] = 0 ; 
+    }
+
+    let i ; 
+
+    if(s1.length != s2.length) 
+    {
+      return false ; 
+    }
+
+
+    for(i = 0 ; i < s1.length ; i++) 
+    {
+      count[s1[i].charCodeAt(0) - 
+      'a'.charCodeAt(0)]++;
+      count[s2[i].charCodeAt(0) - 
+      'a'.charCodeAt(0)]--;
+    }
+
+
+    for(i = 0 ; i < NO_OF_CHARS ; i++) 
+    {
+      if(count[i] != 0) 
+      {
+        return false ; 
+      }  
+    }
+
+    return true ; 
 }
 
 module.exports = isAnagram;
