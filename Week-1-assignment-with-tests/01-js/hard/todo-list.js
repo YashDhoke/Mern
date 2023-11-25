0fb6d3e7df88f4f9296577a8bbaa158a64f78bba
YashDhoke <yashdhoke1@gmail.com>
2023-11-25 20:05:13 +0530

    done

--- Week-1-assignment-with-tests/01-js/hard/todo-list.js
+++ Week-1-assignment-with-tests/01-js/hard/todo-list.js
@@ -12,7 +12,69 @@
 */
 
 class Todo {
+  // Constructor to initialize the todo list
+  constructor() {
+    // Initialize an empty array to store todos
+    this.todos = [];
+  }
 
+  // Method to add a new todo to the list
+  add(todo) {
+    // Push the provided todo to the todos array
+    this.todos.push(todo);
+  }
+
+  // Method to remove a todo at a specific index from the list
+  remove(indexOfTodo) {
+    // Check if the provided index is valid
+    if (indexOfTodo >= 0 && indexOfTodo < this.todos.length) {
+      // Remove the todo at the specified index using splice
+      this.todos.splice(indexOfTodo, 1);
+    } else {
+      // Throw an error for an invalid index
+      throw new Error("Invalid index");
+    }
+  }
+
+  // Method to update a todo at a specific index in the list
+  update(index, updatedTodo) {
+    // Check if the provided index is valid
+    if (index >= 0 && index < this.todos.length) {
+      // Update the todo at the specified index with the provided value
+      this.todos[index] = updatedTodo;
+    } else {
+      // Throw an error for an invalid index
+      throw new Error("Invalid index");
+    }
+  }
+
+  // Method to get all todos in the list
+  getAll() {
+    // Return the entire list of todos
+    return this.todos;
+  }
+
+  // Method to get a todo at a specific index from the list
+  get(indexOfTodo) {
+    // Check if the provided index is valid
+    if (indexOfTodo >= 0 && indexOfTodo < this.todos.length) {
+      // Return the todo at the specified index
+      return this.todos[indexOfTodo];
+    } else {
+      // Throw an error for an invalid index
+      throw new Error("Invalid index");
+    }
+  }
+
+  // Method to clear all todos from the list
+  clear() {
+    // Set the todos array to an empty array
+    this.todos = [];
+  }
 }
+
+// Export the Todo class to make it available for external use
+module.exports = Todo;
+
 
 module.exports = Todo;
