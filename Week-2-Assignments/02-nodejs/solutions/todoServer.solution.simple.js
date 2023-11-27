ae8cbab607498aee899899c2201334f15fbb7717
YashDhoke <yashdhoke1@gmail.com>
2023-11-27 18:51:43 +0530

    todo server changes

--- Week-2-Assignments/02-nodejs/solutions/todoServer.solution.simple.js
+++ Week-2-Assignments/02-nodejs/solutions/todoServer.solution.simple.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-
+const cors = require("cors") ;
 const app = express();
 
 app.use(bodyParser.json());
+app.use(cors()) ; 
 
 let todos = [];
 
@@ -26,14 +27,7 @@ app.get('/todos', (req, res) => {
   res.json(todos);
 });
 
-app.get('/todos/:id', (req, res) => {
-  const todoIndex = findIndex(todos, parseInt(req.params.id));
-  if (todoIndex === -1) {
-    res.status(404).send();
-  } else {
-    res.json(todos[todoIndex]);
-  }
-});
+
 
 app.post('/todos', (req, res) => {
   const newTodo = {
@@ -45,16 +39,7 @@ app.post('/todos', (req, res) => {
   res.status(201).json(newTodo);
 });
 
-app.put('/todos/:id', (req, res) => {
-  const todoIndex = findIndex(todos, parseInt(req.params.id));
-  if (todoIndex === -1) {
-    res.status(404).send();
-  } else {
-    todos[todoIndex].title = req.body.title;
-    todos[todoIndex].description = req.body.description;
-    res.json(todos[todoIndex]);
-  }
-});
+
 
 app.delete('/todos/:id', (req, res) => {
   const todoIndex = findIndex(todos, parseInt(req.params.id));
@@ -71,4 +56,5 @@ app.use((req, res, next) => {
   res.status(404).send();
 });
 
+app.listen(3000) ; 
 module.exports = app;
