56176de8afdddbfaa33a0d7e1c11292041663f8f
YashDhoke <yashdhoke1@gmail.com>
2023-12-17 20:15:14 +0530

    written the frontend part

---
+++ Coursify/Backend/routes/admin.js
@@ -0,0 +1,99 @@
+const mongoose = require("mongoose");
+const express = require('express');
+const { User, Course, Admin } = require("../db");
+const jwt = require('jsonwebtoken');
+const { SECRET } = require("../middleware/auth")
+const { authenticateJwt } = require("../middleware/auth");
+
+const router = express.Router();
+
+router.get("/me" , authenticateJwt , async(req , res) => {
+    const admin = await Admin.findOne( { username : req.user.username}) ; 
+    if(!admin) 
+    {
+        res.status(403).json({message : "Admin not found"}) 
+        return 
+    }
+    res.json({
+        username : admin.username 
+    })
+})
+
+router.post("/signup" , (req , res) => {
+    const {username , password}  = req.body ; 
+    function callback(admin){
+        if(admin){
+            res.status(403).json({message : "Admin already exists"}) ; 
+        }else{
+            const obj = {username : username , password : password} ; 
+            const newAdmin = new Admin(obj) ; 
+            newAdmin.save() ; 
+
+            const token = jwt.sign({username , role : "admin"} , SECRET , {expiresIn : '1h'}) ; 
+            res.json({message : "Admin created successfully" , token}) ; 
+        }
+    }
+    Admin.findOne({username}).then(callback) ; 
+})
+
+router.post("/login" , async(req,res) => {
+    const {username , password} = req.body ; 
+    const admin = await Admin.findOne({username , password}) ; 
+    if(admin){
+        jwt.sign({username , role : "admin"}, SECRET , {expiresIn : "1h"}) ; 
+        res.json({message : "Logged in successfully!"} , token) ; 
+    } else {
+        res.status(403).json({message : "Invalid Credentials"}) ; 
+    }
+}); 
+
+router.post("/courses" , authenticateJwt , async (req,res) => {
+    const courses = new Course(req.body) ; 
+    await courses.save() ; 
+    res.json({message : "Course created successfully!" , courseId : courses.id}) ; 
+});
+
+router.put("/courses" , authenticateJwt , async (req,res) => {
+    const course = await Course.findByIdAndUpdate(req.params.courseId , req.body , {new : true}) ; 
+    if(course){
+        res.json({message : "Course edited successfully!"}) ; 
+    }
+    else{
+        res.status(404).json({message : "Course not found"}) ; 
+    } 
+});
+
+router.get("/courses", authenticateJwt , async(req , res) {
+    const courses = await Course.find({}) ; 
+    res.json({courses}) ; 
+});
+
+router.get("/course/:courseId" , authenticateJwt , async (req , res) => {
+    const courseId = req.params.courseId  ; 
+    const course = await Course.findById(courseId) ;
+    if(course){
+        res.json({message : "course not found"}) ; 
+    }else {
+        res.json({course}) ; 
+    }
+}) ; 
+
+module.exports = router ;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
